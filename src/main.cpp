f9e8859717b3f34c2c9d2b6e6e76b7c20e3523fd
Bernd Lamecker <bernd.lamecker@basyskom.de>
2010-03-10 14:30:38 +0100

    Changes: Set property for not styling the QGraphicsView
    RevBy:TrustMe

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@
 #include <QGLWidget>
 #include "duicompositescene.h"
 #include "duicompositemanager.h"
+#include <DuiNamespace>
 
 int main(int argc, char *argv[])
 {
@@ -29,6 +30,7 @@ int main(int argc, char *argv[])
     QGraphicsScene *scene = app.scene();
     QGraphicsView view(scene);
 
+    view.setProperty(Dui::NoDuiStyle, true);
     view.setUpdatesEnabled(false);
     view.setAutoFillBackground(false);
     view.setBackgroundBrush(Qt::NoBrush);
