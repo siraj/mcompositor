f2e58f6080b0cad818ff2d0c4e782fbda93f74ad
Adam Endrodi <ext-adam.endrodi@nokia.com>
2010-12-31 18:26:05 +0200

    restore accidentally removed translations
    (by 702f7dfc0ac0807a21e0f0b50a403c9f85e99a2c) and update line numbers again

--- translations/recovery.ts
+++ translations/recovery.ts
@@ -4,22 +4,22 @@
 <context>
     <name></name>
     <message id="qtn_reco_app_not_responding">
-        <location filename="../decorators/mdecorator/mdecoratorwindow.cpp" line="248"/>
+        <location filename="../decorators/mdecorator/mdecoratorwindow.cpp" line="241"/>
         <source></source>
-        <translation type="unfinished"></translation>
+        <translation>%1 sucks</translation>
     </message>
     <message id="qtn_reco_close_app_question">
-        <location filename="../decorators/mdecorator/mdecoratorwindow.cpp" line="249"/>
+        <location filename="../decorators/mdecorator/mdecoratorwindow.cpp" line="242"/>
         <source></source>
-        <translation type="unfinished"></translation>
+        <translation>Do you want to close it?</translation>
     </message>
     <message id="qtn_comm_command_yes">
-        <location filename="../decorators/mdecorator/mdecoratorwindow.cpp" line="251"/>
+        <location filename="../decorators/mdecorator/mdecoratorwindow.cpp" line="244"/>
         <source></source>
         <translation type="unfinished"></translation>
     </message>
     <message id="qtn_comm_command_no">
-        <location filename="../decorators/mdecorator/mdecoratorwindow.cpp" line="253"/>
+        <location filename="../decorators/mdecorator/mdecoratorwindow.cpp" line="246"/>
         <source></source>
         <translation type="unfinished"></translation>
     </message>
